1a9948cc728843feef8975f7bbef7115a0db7688
Atharvasawant09 <atharva.sawant1919@gmail.com>
2024-10-17 00:48:22 +0530

    changes

---
+++ my_portfolio/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Contact from "@/components/Contact";
+import Content from "@/components/Content";
+
+export default function Home() {
+  return (
+    <div>
+    <Content/>
+    <Contact/>
+    </div>
+  );
+}
---
+++ my_portfolio/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <>
+    
+     <div className='text-5xl text-white flex flex-col justify-center items-center '> Elevating User Experiences, One Component at a Time.</div>
+     <div className='text-xl text-white flex flex-col justify-center items-center '> Hi, I am Atharva Sawant</div>
+     
+    </>
+  )
+}
+
+export default Content
