e0f109a53d68507ae91c626686bc512ba7650527
Atharvasawant09 <atharva.sawant1919@gmail.com>
2024-11-08 16:04:31 +0530

    changes

--- my_portfolio/components/About.js
+++ my_portfolio/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="p-8">
       <h1 className='text-4xl font-bold text-white text-center'>About Me</h1>
       <p className='text-lg text-white pl-80 pr-80 pt-9'>
-        Hello! I’m Atharva Sawant, a third-year B.Tech Computer Science student and a dedicated full-stack developer with a strong focus on crafting efficient and engaging web applications. With experience in modern web development technologies such as React, Next.js, Node.js, Express.js, and MongoDB, I thrive on building solutions that are both user-friendly and scalable.
+        Hello! I'm Atharva Sawant, a third-year B.Tech Computer Science student and a dedicated full-stack developer with a strong focus on crafting efficient and engaging web applications. With experience in modern web development technologies such as React, Next.js, Node.js, Express.js, and MongoDB, I thrive on building solutions that are both user-friendly and scalable.
       </p>
       <p className='text-lg text-white pl-80 pr-80 pt-9'>
         I am always eager to learn new technologies, solve complex problems, and bring innovative ideas to life. My projects reflect my commitment to quality and a passion for technology-driven solutions that elevate user experiences. Whether it's backend logic or frontend interfaces, I aim to deliver well-rounded, high-performance applications.
